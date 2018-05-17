cbbc40adcbd76925865e3a5cc228f6283e30a750
Emily Zuo <xiang.zuo@teradata.com>
2018-05-16 17:13:13 -0700

    first push

---
+++ numpy_pandas/numpy_examples.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+# matrix attributes
+array = np.array([[1,2,3],[4,5,6]])
+print (array)
+# print dimension
+print('num of dim: ', array.ndim)
+# print shape
+print ('shape: ', array.shape)
+# print size
+print ('size: ', array.size)
+
